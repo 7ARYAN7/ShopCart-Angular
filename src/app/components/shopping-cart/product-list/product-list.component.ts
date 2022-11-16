a94bacec22fdec9dc862a0abfc50d357fb7cd7b4
7ARYAN7 <aryan1275.cse19@chitkara.edu.in>
2022-11-16 16:27:29 +0530

    before filter problem

--- src/app/components/shopping-cart/product-list/product-list.component.ts
+++ src/app/components/shopping-cart/product-list/product-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {ProductService} from "../../../service/product.service";
 import {Product} from "../../../model/product";
 import {Filter} from "../../../model/filter";
+import {LabelType, Options} from "ng5-slider";
+import {HttpErrorResponse} from "@angular/common/http";
 
 
 @Component({
@@ -12,6 +14,7 @@ import {Filter} from "../../../model/filter";
 export class ProductListComponent implements OnInit {
   productList: Product[] = [];
   search:string;
+  productNF : boolean;
   model: any = {};
   constructor(private productService: ProductService) {
   }
@@ -27,8 +30,10 @@ export class ProductListComponent implements OnInit {
       })
   }
   searcH(){
-    if(this.model.search==='')
+    this.productNF= false;
+    if(this.model.search ==='' || this.model.search=== undefined) {
       this.loadProducts();
+    }
     else {
       this.productService.searchProductByString(this.model.search).subscribe((products) => {
         this.productList = products;
@@ -37,10 +42,35 @@ export class ProductListComponent implements OnInit {
   }
 
   loadFilteredProducts(filter: Filter, category: string){
-    this.productService.findByFilter(filter,category).subscribe((products)=>{
-      this.productList= products;
-      console.log(this.productList[0].name);
 
-    })
+  }
+  minValue: number = 50000;
+  maxValue: number = 250000;
+  options: Options = {
+    floor: 0,
+    ceil: 300000,
+    translate: (value: number, label: LabelType): string => {
+      switch (label) {
+        case LabelType.Low:
+          return '<b>Min Price:</b> ₹' + value;
+        case LabelType.High:
+          return '<b>Max Price:</b> ₹' + value;
+        default:
+          return '₹' + value;
+      }
+    }
+  };
+
+  filterby(){
+    console.log(this.model.categoryL)
+    let filter = new Filter(this.minValue.toString(),this.maxValue.toString(),this.model.subcategory,this.model.brand)
+    this.productService.findByFilter(filter,this.model.categoryL).subscribe((products)=>{
+      this.productList= products;
+      this.productNF = false;
+      console.log(this.productList[0].name);
+    },(error:HttpErrorResponse)=>{
+      this.productNF = true;
+      }
+      )
   }
 }
